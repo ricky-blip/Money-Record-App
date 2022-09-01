e65b6a4ff02e46005e0ba6070086c5672e893c41
ricky-blip <ricky.rinaldy77@gmail.com>
2022-09-01 12:15:49 +0700

    update

--- money_record/lib/pages/register.dart
+++ money_record/lib/pages/register.dart
@@ -14,57 +14,95 @@ class RegisterPages extends StatelessWidget {
             children: [
               Column(
                 children: [
+                  //SECTION 1:IMAGE
                   Image.asset(
                     "assets/ic_money.png",
                     scale: 5,
                     color: Colors.lightGreen,
                   ),
                   SizedBox(height: 40),
+                  //SECTION 2: TEXT FIELD
                   Column(
                     children: [
+                      //NOTE Name
                       TextFormField(
                         decoration: InputDecoration(
                           labelText: 'Name',
+                          labelStyle: TextStyle(color: Colors.green),
                           hintText: "type name",
-                          border: OutlineInputBorder(),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide:
+                                BorderSide(color: Colors.lightGreen, width: 2),
+                          ),
                         ),
                       ),
                       SizedBox(height: 20),
+                      //NOTE Email
                       TextFormField(
                         decoration: InputDecoration(
                           labelText: 'Email',
+                          labelStyle: TextStyle(color: Colors.green),
                           hintText: "type email",
-                          border: OutlineInputBorder(),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide:
+                                BorderSide(color: Colors.lightGreen, width: 2),
+                          ),
                         ),
                       ),
                       SizedBox(height: 20),
+                      //NOTE Password
                       TextFormField(
                         decoration: InputDecoration(
                           labelText: 'Password',
+                          labelStyle: TextStyle(color: Colors.green),
                           hintText: "type password",
-                          border: OutlineInputBorder(),
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide:
+                                BorderSide(color: Colors.lightGreen, width: 2),
+                          ),
                         ),
                       ),
                       SizedBox(height: 40),
                     ],
                   ),
-                  BottomAppBar(
-                    elevation: 10,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.lightGreen,
-                        // fixedSize: Size(177, 56),
-                        minimumSize:
-                            Size(MediaQuery.of(context).size.width, 50),
-                        maximumSize:
-                            Size(MediaQuery.of(context).size.width, 50),
-                        textStyle: TextStyle(fontSize: 24),
+                  //SECTION 3: BUTTON
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: BottomAppBar(
+                      elevation: 10,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          // shape: StadiumBorder(),
+                          primary: Colors.lightGreen,
+                          minimumSize: Size(
+                            MediaQuery.of(context).size.width,
+                            50,
+                          ),
+                          maximumSize: Size(
+                            MediaQuery.of(context).size.width,
+                            50,
+                          ),
+                          textStyle: TextStyle(fontSize: 24),
+                        ),
+                        onPressed: () {},
+                        child: Text("Register"),
                       ),
-                      onPressed: () {},
-                      child: Text("Register"),
                     ),
                   ),
                   SizedBox(height: 20),
+                  //SECTION 4: NOTE
                   Row(
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
